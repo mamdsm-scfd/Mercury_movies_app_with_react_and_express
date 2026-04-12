a7b7457dcd8420a9adff5b8f108c332180393a70
Mahmoud <mdohair22@gmail.com>
2026-04-12 16:12:08 +0300

    build hero section UI #6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import movies from './mockData'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world gggggggggg</h1>
+      <Navbar></Navbar>
+      <Hero 
+        movie={movies[0]} 
+        onPrev={() => console.log("prev")}
+        onNext={() => console.log("next")}
+      />
     </div>
   )
 }
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,49 @@
+import "./Hero.css";
+
+function Hero({ movie, onPrev, onNext }) {
+
+    if (!movie) return null
+
+  const minutes = movie.runtime;
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+
+
+  return (
+    <div className="hero">
+      <div
+        className="hero-bg"
+        style={{ backgroundImage: `url(${movie.backdrop_path})` }}
+      />
+
+      <div className="hero-gradient"></div>
+
+      <div className="hero-overlay" />
+
+      <div className="hero-content">
+        <h1 className="hero-title">{movie.title}</h1>
+        <div className="hero-meta">
+          <span className="imdb-badge">IMDB</span>
+          <span>{movie.vote_average}</span>
+          <span className="dot">•</span>
+          <span>{new Date(movie.release_date).getFullYear()}</span>
+          <span className="dot">•</span>
+          <span>
+            {hours}h {mins}min
+          </span>
+          <span className="dot">•</span>
+          <span>{movie.genres}</span>
+        </div>
+        <p className="hero-desc">{movie.overview}</p>
+      </div>
+
+      <div className="hero-nav">
+        <button className="nav-btn" onClick={onPrev}>&#8249;</button>
+        <button className="nav-btn" onClick={onNext}>&#8250;</button>
+      </div>
+    </div>
+  );
+}
+
+
+export default Hero;
